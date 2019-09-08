115c6e9cfbc6a02f1cb71cafba09c0a51c53f672
Trung1234 <trungsieunhan24@gmail.com>
2019-09-08 12:20:58 +0700

    add automapper example

---
+++ AutomapperExampler/AutomapperExampler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomapperExampler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
