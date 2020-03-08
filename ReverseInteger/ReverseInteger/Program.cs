27788fabdd5beb7c3c65f338a4e3b2ff5ac688cc
unknown <Trung1234>
2020-03-07 23:45:20 -0800

    update logic

--- ReverseInteger/ReverseInteger/Program.cs
+++ ReverseInteger/ReverseInteger/Program.cs
@@ -6,36 +6,25 @@ namespace ReverseInteger
     {
         static void Main(string[] args)
         {          
-            int reverseNumber = reverseInteger(34566);
-            Console.WriteLine("reversed Number of 34566 is {0}", reverseNumber);
-            reverseNumber = reverseInteger(-10607);
-            Console.WriteLine("reversed Number of -10607 is {0}", reverseNumber);
-            reverseNumber = reverseInteger(1020);
-            Console.WriteLine("reversed Number of 1020 is {0}", reverseNumber);
+            int reverseNumber = reverseInteger(340566);
+            Console.WriteLine("reversed Number of 340566 is {0}", reverseNumber);
+            reverseNumber = reverseInteger(-100607);
+            Console.WriteLine("reversed Number of -100607 is {0}", reverseNumber);
+            reverseNumber = reverseInteger(10020);
+            Console.WriteLine("reversed Number of 10020 is {0}", reverseNumber);
             Console.ReadLine();
         }
-        static int reverseInteger(int x)
+        static int reverseInteger(int num)
         {
-            int result = 0;
-            while (x % 10 == 0)
+            int  reversed = 0;
+
+            while (num != 0)
             {
-                x = x / 10;
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
             }
-            if(x % 10 != 0)
-            {
-                while (x % 10 != 0)
-                {
-                    int n = x % 10;
-                    int length = (int) Math.Abs(x).ToString().Length;
-                    result += n * (int)Math.Pow(10, length - 1);
-                    x = x / 10;
-                    if(length > 1 && x % 10 == 0)
-                    {
-                        x = x / 10;
-                    }
-                }
-            }          
-            return result;
+            return reversed;
         }
     }
 }
