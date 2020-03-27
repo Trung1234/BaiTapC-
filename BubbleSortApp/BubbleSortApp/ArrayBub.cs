ffbf2225903f6774beb8004b5369c2a756f0a158
unknown <Trung1234>
2020-03-27 08:24:24 -0700

    add buble sort app

---
+++ BubbleSortApp/BubbleSortApp/ArrayBub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSortApp
+{
+    public class ArrayBub
+    {
+        private long[] a; // ref to array a
+        private int nElems; // number of data items
+        // create constructor
+        public ArrayBub(int max)
+        {
+            a = new long[max]; // create the array
+            nElems = 0; // no item yet
+        }
+        public void Add(long value)
+        {
+            a[nElems] = value;
+            nElems++;
+        }
+        public void Display()
+        {
+            for(int i = 0; i < nElems; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public int Size()
+        {
+            return nElems;
+        }
+        public void BubbleSort()
+        {
+            for(int i = nElems - 1; i > 1; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        //swap
+                        long temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BubbleSortApp/BubbleSortApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace BubbleSortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayBub data = new ArrayBub(100);
+            data.Add(20);
+            data.Add(40);
+            data.Add(30);
+            data.Add(4);
+            data.Add(5);
+            data.Add(25);
+            Console.WriteLine("Array before sort!");
+            data.Display();
+            data.BubbleSort();
+            Console.WriteLine("Array after sort!");
+            data.Display();
+            Console.ReadLine();
+        }
+    }
+}
