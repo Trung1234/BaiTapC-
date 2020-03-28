541359af43b54f1db4c084ebd2910b4cd5c1431c
unknown <Trung1234>
2020-03-27 22:04:03 -0700

    add Contain function

--- BubbleSortApp/BubbleSortApp/ArrayBub.cs
+++ BubbleSortApp/BubbleSortApp/ArrayBub.cs
@@ -6,18 +6,65 @@ namespace BubbleSortApp
 {
     public class ArrayBub
     {
+        
         private long[] a; // ref to array a
         private int nElems; // number of data items
+        public int Count 
+        {
+            get
+            {
+                return nElems;
+            }
+        }
         // create constructor
         public ArrayBub(int max)
         {
             a = new long[max]; // create the array
             nElems = 0; // no item yet
         }
+
+        public void AddFirst(long value)
+        {          
+            nElems++;
+            // move higher ones down
+            for (int k = nElems-1; k >0; k--)
+                a[k] = a[k-1];
+            a[0] = value;
+        }
         public void Add(long value)
         {
             a[nElems] = value;
             nElems++;
+        }
+
+        public bool Delete(long value)
+        {
+            int j;
+            // look for it
+            for (j = 0; j < nElems; j++)
+                if (a[j] == value)
+                    break;
+            if (j == nElems)
+                return false;
+            else // found it
+            {
+                // move higher ones down
+                for (int k = j; k < nElems; k++)
+                    a[k] = a[k + 1];
+                nElems--; // decrement size
+                return true;
+            }
+        }
+        public bool Contains(long searchKey)
+        {
+            int j;
+            for (j = 0; j < nElems; j++)
+                if (a[j] == searchKey)
+                    break;
+            if (j == nElems)
+                return false;
+            else
+                return true;
         }
         public void Display()
         {
@@ -27,10 +74,7 @@ namespace BubbleSortApp
             }
             Console.WriteLine();
         }
-        public int Size()
-        {
-            return nElems;
-        }
+
         public void BubbleSort()
         {
             for(int i = nElems - 1; i > 1; i--)
--- BubbleSortApp/BubbleSortApp/Program.cs
+++ BubbleSortApp/BubbleSortApp/Program.cs
@@ -15,10 +15,17 @@ namespace BubbleSortApp
             data.Add(5);
             data.Add(25);
             Console.WriteLine("Array before sort!");
+            Console.WriteLine("Size of array: {0}", data.Count);
             data.Display();
             data.BubbleSort();
             Console.WriteLine("Array after sort!");
+            Console.WriteLine("Size of array: {0}", data.Count);
             data.Display();
+            Console.WriteLine("Array after Add first!");
+            
+            data.AddFirst(100);
+            data.Display();
+            Console.WriteLine("Size of array: {0}", data.Count);
             Console.ReadLine();
         }
     }
